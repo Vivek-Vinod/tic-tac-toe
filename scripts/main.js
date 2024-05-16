2f7b334b122118a59d4cb772889725c94107fa27
Vivek Vinod <vivekvinodchess@gmail.com>
2024-05-16 12:58:41 +0530

    refactor random move into function

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const gameBoard = document.querySelector(".gameBoard");
 const gameTable = [];
-const result = document.querySelector("p");
+const result = document.querySelector(".result");
 
 for (let i = 0; i < 3; i++) {
     const gameRow = [];
@@ -19,38 +19,51 @@ function main() {
         for (const gameCell of gameRow) {
             gameCell.addEventListener("click", (e) => {
                 if (e.target.getAttribute("class") === "empty") {
+                    // make player move
                     const marker = document.createElement("img");
                     const markerSrc = getMarkerSrc(player);
                     marker.setAttribute("src", markerSrc);
                     gameCell.appendChild(marker);
                     e.target.setAttribute("class", player);
                     if (isWin(player)) {
-                        result.textContent = "win";
+                        result.textContent = `${player} wins the game!`;
+                        return;
                     }
                     player = changePlayer(player);
+
                     // make a random move by the computer
-                    outerLoop: 
-                    for (const gameRow of gameTable) {
-                        for (const gameCell of gameRow) {
-                            if (gameCell.getAttribute("class") === "empty") {
-                                const marker = document.createElement("img");
-                                const markerSrc = getMarkerSrc(player);
-                                marker.setAttribute("src", markerSrc);
-                                gameCell.appendChild(marker);
-                                gameCell.setAttribute("class", player);
-                                if (isWin(player)) {
-                                    result.textContent = "win";
-                                }
-                                player = changePlayer(player);
-                                break outerLoop;
-                            }
-                        }
-                    }
+                    makeRandomMove(player);
+                    player = changePlayer(player);
                 }
             });
         }
     }
 }
+
+function makeRandomMove (player) {
+    // make an array of empty cells
+    const emptyCells = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (gameTable[i][j].getAttribute("class") === "empty") {
+                emptyCells.push(gameTable[i][j]);
+            }
+        }
+    }
+    console.log(emptyCells);
+    // choose a random cell from empty cells
+    const rndNum = Math.floor(Math.random() * emptyCells.length);
+    console.log(rndNum);
+    const gameCell = emptyCells[rndNum];
+    const marker = document.createElement("img");
+    const markerSrc = getMarkerSrc(player);
+    marker.setAttribute("src", markerSrc);
+    gameCell.appendChild(marker);
+    gameCell.setAttribute("class", player);
+    if (isWin(player)) {
+        result.textContent = `${player} wins the game!`;
+    }
+} 
 
 function isWin(player) {
     for (let i = 0; i < 3; i++) {
@@ -63,6 +76,7 @@ function isWin(player) {
             return true;
         }
     }
+
     // check for win in diagonals
     if (gameTable[0][0].getAttribute("class") === player && gameTable[1][1].getAttribute("class") === player && gameTable[2][2].getAttribute("class") === player) {
         return true;
